c43dc7ce2821efa3153fa3a2fcf2217c427568ed
mgeldi <muhammed_ali96@web.de>
2022-04-17 14:23:06 +0200

    Add exception for user already existing

---
+++ src/main/java/de/mgeldi/thiccthocc/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package de.mgeldi.thiccthocc.exceptions;
+
+public class UserAlreadyExistsException extends Exception {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
